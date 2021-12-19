e1a27448685b98e7325db0f5cf1d9a9d445f95a1
hindbenazzi <hindb788@gmail.com>
2021-12-19 15:37:22 +0100

    add jwt auth basics

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,20 +5,35 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-    private $manager;
-    public function __construct(ManagerRegistry $registry,EntityManagerInterface $manager)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
-        $this->manager = $manager;
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(UserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
     }
 
     // /**
@@ -49,20 +64,4 @@ class UserRepository extends ServiceEntityRepository
         ;
     }
     */
-    public function saveUser($newUser)
-    {
-        $this->manager->persist($newUser);
-        $this->manager->flush();
-    }
-         public function updateUser(User $user): User
-        {
-           $this->manager->persist($user);
-           $this->manager->flush();
-           return $user;
-        } 
-        public function removeUser(User $user)
-        {
-            $this->manager->remove($user);
-            $this->manager->flush();
-        }
 }
